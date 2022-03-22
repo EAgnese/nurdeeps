d14f7e4a0454d43defa55a9f3242c20a0371ca38
= <=>
2022-03-22 14:55:25 +0100

    rangement fichiet

--- api/api.js
+++ api/api.js
@@ -12,21 +12,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
-const gamesRouter = require('./api/routes/games_route');
-const gameTypesRouter = require('./api/routes/game_types_route');
-// const suggestionsRouter = require('./api/routes/suggestions_route');
-const runsRouter = require('./api/routes/runs_route');
-const usersRouter = require('./api/routes/users_route');
-const platformsRouter = require('./api/routes/platforms_route');
-// const comptesRouter = require('./api/routes/comptes_route');
+const gamesRouter = require('./routes/games_route');
+const gameTypesRouter = require('./routes/game_types_route');
+const suggestionsRouter = require('./api/routes/suggestions_route');
+const runsRouter = require('./routes/runs_route');
+const runCatsRouter = require('./routes/run_categories_route');
+const usersRouter = require('./routes/users_route');
+const platformsRouter = require('./routes/platforms_route');
 
 app.use('/api/games', gamesRouter);
 app.use('/api/game_types', gameTypesRouter);
-// app.use('/api/suggestions', suggestionsRouter);
+app.use('/api/suggestions', suggestionsRouter);
 app.use('/api/runs', runsRouter);
+app.use('/api/run_categories', runCatsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/platforms', platformsRouter);
-// app.use('/api/comptes', comptesRouter);
 
 //The 404 Route (ALWAYS Keep this as the last route)
 app.get('*', (req, res) => {
