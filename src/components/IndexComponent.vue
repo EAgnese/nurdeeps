c44bb44695f803a38ad2c078a7fb0da7f4903cd8
Agnese <neutron34980@gmail.com>
2022-03-31 03:38:16 +0200

    changement index

--- src/components/IndexComponent.vue
+++ src/components/IndexComponent.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>Téma la page de base</p>
+    <div>
+        <h1>Bienvenue!</h1>
+        <p>Vous voici sur le site nurdeeps</p>
+        <p>Cliquer sur games pour voir tous les jeux enregistrés</p>
+        <p>Cliquer sur suggestions pour voir les idées  d'autres utilisateurs</p>
+    </div>
 </template>
+
+<style scoped>
+    div{
+        text-align: center;
+    }
+</style>
